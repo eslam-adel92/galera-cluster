6e55120145a579bacb86a407cc29a2090e13fda6
eslam-adel92 <prog.eslamadel@gmail.com>
2019-05-22 13:25:28 +0200

    Add a condition in the entrypoint.sh

--- docker-compose/entry_point.sh
+++ docker-compose/entry_point.sh
@@ -4,12 +4,16 @@ RESULT=`echo $?`
 ##sleep 50
 VALUE=`mysql -u root -p${MYSQL_PASSWORD} -e "SHOW GLOBAL STATUS LIKE 'wsrep_ready';" | awk 'FNR == 2 {print $2}'`
 
-if [[ RESULT -ne '0' || "$VALUE" == "OFF" ]]
+if [[ "$VALUE" == "ON" ]]
 then
-  kill -9 $(ps aux | grep mysqld | grep -v grep | awk '{print $2}')
-  rm -rf /var/lib/mysql
-  docker-entrypoint.sh mysqld --wsrep-new-cluster
-  echo done
+   echo "The cluster is up and running"
+
+elif [[ RESULT -ne '0' || "$VALUE" == "OFF" ]]
+then
+     kill -9 $(ps aux | grep mysqld | grep -v grep | awk '{print $2}')
+     rm -rf /var/lib/mysql
+     docker-entrypoint.sh mysqld --wsrep-new-cluster
+     echo done
 else
-   echo "The cluster failed or the node failed joining"
+     echo "The cluster failed or the node failed joining"
 fi
